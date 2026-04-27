5c41488180d282b81962f85b0378cb731d4b4174
Bardawil95 <asquelon22@hotmail.com>
2026-04-27 19:00:28 +0100

    Feature: add calculator core with overflow protection

---
+++ CalculatorTest/Calculator.Core/ISimpleCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator.Core
+{
+    public interface ISimpleCalculator
+    {
+        int Add(int start, int amount);
+        int Subtract(int start, int amount);
+    }
+}
---
+++ CalculatorTest/Calculator.Core/SimpleCalculator.cs
@@ -0,0 +1,22 @@
+﻿namespace Calculator.Core
+{
+    public class SimpleCalculator : ISimpleCalculator
+    {
+        // checked is used for arithmatic overflow checking
+        public int Add(int start, int amount)
+        {
+            checked
+            {
+                return start + amount;
+            }
+        }
+
+        public int Subtract(int start, int amount)
+        {
+            checked
+            {
+                return start - amount;
+            }
+        }
+    }
+}
